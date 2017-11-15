950c50a088e971682123fb71c7ea1a9ea5d2f7dc
Prashanth Naika <c.prashanth.naika@uptake.com>
2017-11-14 19:02:55 -0600

    images and stuff

--- src/HomePage/Components/HomePage.js
+++ src/HomePage/Components/HomePage.js
@@ -1,8 +1,42 @@
+import './homepage.scss';
 import React, { Component } from 'react';
+import { Input, Image, Button } from 'semantic-ui-react';
 
 export class HomePage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+           searchValue: ''
+        };
+        this.searchSerial = this.searchSerial.bind(this);
+        this.getSearchDetails = this.getSearchDetails.bind(this);
+    }
+
+    searchSerial(e) {
+        this.setState({
+            searchValue: e.target.value
+        });
+    }
+
+    getSearchDetails() {
+        console.log(this.state.searchValue)
+    }
     render() {
-        return (<div>This is Home Page</div>);
+        return (
+            <div className="home-page">
+                <Image src="../../_app/images/Logo-IV-Vapid.png" size="small" className="logo-size"/>
+                <div className="search-input">
+                    <div>Search by Serial Number or Blockchain ID:</div>
+                    <input
+                        placeholder="Search..."
+                        className='search-bar'
+                        onChange={this.searchSerial}
+                    />
+                    <button className='btn go-button' onClick={this.getSearchDetails}>Go</button>
+                </div>
+            </div>
+        );
     }
 }
 
--- src/Main/actions.js
+++ src/Main/actions.js
@@ -40,3 +40,68 @@ export function decrementLoadingCount() {
 export function resetLoading() {
     return { type: RESET_LOADING_COUNT };
 }
+
+
+// import 'whatwg-fetch';
+// import {
+//     SET_JOBSITE_REQUEST_LIST,
+//     SET_JOBSITE_REQUEST_LOADING
+// } from '../_app/actionTypes';
+// import { getDealerHeaders, checkStatus, parseJSON } from '../_app/utils';
+// import { GATEKEEPER_API } from '../_app/constants';
+// import { actions as toastActions } from '../Toaster';
+// import qs from 'query-string';
+//
+// export function setJobsiteRequestList(requests) {
+//     return {
+//         type: SET_JOBSITE_REQUEST_LIST,
+//         payload: { requests }
+//     };
+// }
+//
+// export function setJobsiteRequestLoading(isLoading) {
+//     return {
+//         type: SET_JOBSITE_REQUEST_LOADING,
+//         payload: { isLoading }
+//     };
+// }
+//
+// export function getJobsiteParams(status) {
+//     const queryParams = {
+//         namespace: 'jobsite'
+//     };
+//     switch (status) {
+//         case 'All':
+//             break;
+//         case 'Open':
+//             queryParams.status = 'OPENED';
+//             break;
+//         case 'Completed':
+//             queryParams.status = 'CLOSED';
+//             break;
+//     }
+//
+//     return qs.stringify(queryParams);
+// }
+//
+// export function callJobsiteRequest(dealerCode, jobsiteStatus) {
+//     const url = `${GATEKEEPER_API}/requests?${getJobsiteParams(jobsiteStatus)}`;
+//     return (dispatch) => {
+//         dispatch(setJobsiteRequestLoading(true));
+//         return fetch(url, {
+//             headers: getDealerHeaders(dealerCode)
+//         })
+//             .then(checkStatus)
+//             .then(parseJSON)
+//             .then((res) => {
+//                 dispatch(setJobsiteRequestList(res));
+//             })
+//             .catch((err) => {
+//                 dispatch(toastActions.setToasterMessage(err));
+//             })
+//             .then(() => {
+//                 dispatch(setJobsiteRequestLoading(false));
+//             });
+//     };
+// }
+
